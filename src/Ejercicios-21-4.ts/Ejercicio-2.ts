bc6551fda90be1bf23c1e1ae538259a6b843eefa
AlvaroCorreaD <104107312+AlvaroCorreaD@users.noreply.github.com>
2022-04-24 17:29:29 -0300

    Agrego Ejercicio 2

---
+++ src/Ejercicios-21-4.ts/Ejercicio-2.ts
@@ -0,0 +1,12 @@
+//Ejercicio Granjera 2
+console.log("Avanzar");
+console.log("Llenar Hoyo");
+console.log("Avanzar");
+console.log("Avanzar");
+console.log("Avanzar");
+console.log("Llenar Hoyo");
+console.log("Avanzar");
+console.log("Eliminar Pila");
+console.log("Avanzar");
+console.log("Eliminar Pila");
+//Ejercicio Completo
