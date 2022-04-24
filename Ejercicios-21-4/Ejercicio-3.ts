d0f7f4ce2cfff4baea8ccf4d32d1ae629d3bd7d5
AlvaroCorreaD <104107312+AlvaroCorreaD@users.noreply.github.com>
2022-04-24 17:41:47 -0300

    Corrijo index.html

--- Ejercicios-21-4/Ejercicio-3.ts
+++ Ejercicios-21-4/Ejercicio-3.ts
@@ -2,8 +2,8 @@
 console.log("Girar a la Izquierda 90°");
 console.log("Avanzar");
 console.log("Girar a la Derecha 90°");
-console.log("Avnazar");
-console.log("Avnazar");
+console.log("Avanzar");
+console.log("Avanzar");
 console.log("Girar a la Derecha 90°");
 console.log("Avanzar");
 console.log("Eliminar Pila");
