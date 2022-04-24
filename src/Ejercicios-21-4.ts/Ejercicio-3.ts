8257bb4bc137f58079254a7b97a4398ea22b18fe
AlvaroCorreaD <104107312+AlvaroCorreaD@users.noreply.github.com>
2022-04-24 17:34:00 -0300

    Agrego Ejercicio

---
+++ src/Ejercicios-21-4.ts/Ejercicio-3.ts
@@ -0,0 +1,10 @@
+// Ejercicio Granjera 3
+console.log("Girar a la Izquierda 90°");
+console.log("Avanzar");
+console.log("Girar a la Derecha 90°");
+console.log("Avnazar");
+console.log("Avnazar");
+console.log("Girar a la Derecha 90°");
+console.log("Avanzar");
+console.log("Eliminar Pila");
+//Ejercicio Completo
