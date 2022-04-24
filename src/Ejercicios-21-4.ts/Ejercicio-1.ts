f048f5130a3e453d0e96f9b89b80c9157f969cfe
AlvaroCorreaD <104107312+AlvaroCorreaD@users.noreply.github.com>
2022-04-24 17:29:57 -0300

    Corrijo

--- src/Ejercicios-21-4.ts/Ejercicio-1.ts
+++ src/Ejercicios-21-4.ts/Ejercicio-1.ts
@@ -19,16 +19,3 @@ console.log("Eliminar Pila");
 console.log("Avanzar");
 console.log("Eliminar Pila");
 //Ejercicio Completo
-
-//Ejercicio Granjera 2
-console.log("Avanzar");
-console.log("Llenar Hoyo");
-console.log("Avanzar");
-console.log("Avanzar");
-console.log("Avanzar");
-console.log("Llenar Hoyo");
-console.log("Avanzar");
-console.log("Eliminar Pila");
-console.log("Avanzar");
-console.log("Eliminar Pila");
-//Ejercicio Completo
