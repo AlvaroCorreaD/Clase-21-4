8d8762e60ae9900a72ddb018a3dbe8552a55f149
AlvaroCorreaD <104107312+AlvaroCorreaD@users.noreply.github.com>
2022-04-25 21:56:59 -0300

    Comentario

--- src/datos.ts
+++ src/datos.ts
@@ -1,6 +1,6 @@
 let btnEnv = document.getElementById("btnEnviar");
 let dato = document.getElementById("dato");
-let rotulo = document.getElementById("rotulo");
+// let rotulo = document.getElementById("rotulo");
 btnEnv.addEventListener("click", () => {
   // TU CODIGO VA AQUI, POR EJEMPLO:
   console.log("el dato ingresado es ", dato.value);
